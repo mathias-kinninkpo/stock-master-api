d4868c26c44e24f1c64a11810857dda71f75354d
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-01-29 19:33:28 +0100

    modification de la date d'expiration du token

--- routes/auth.py
+++ routes/auth.py
@@ -24,7 +24,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=59)
+        expire = datetime.utcnow() + timedelta(days=3)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
