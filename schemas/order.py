be070adf956e90a35d0c7da70e29221f97156f1a
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-01-30 12:27:36 +0100

    add order schema

---
+++ schemas/order.py
@@ -0,0 +1,24 @@
+# schemas/order.py
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class OrderBase(BaseModel):
+    product_id: int
+    quantity: int
+    total_amount: float
+    status: str
+    customer_name: str
+    customer_contact: str
+
+class OrderCreate(OrderBase):
+    pass
+
+class Order(OrderBase):
+    order_id: int
+    order_date: datetime
+
+    class Config:
+        orm_mode = True
+        from_attributes=True
+        
